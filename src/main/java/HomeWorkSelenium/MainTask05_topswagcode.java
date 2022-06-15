a8f552ab5907216dd25f1261b65fec6b13f6a45e
Evikaaaa <evikaaa@gmail.com>
2022-06-15 22:36:07 +0200

    topswagcode

---
+++ src/main/java/HomeWorkSelenium/MainTask05_topswagcode.java
@@ -0,0 +1,34 @@
+package HomeWorkSelenium;
+
+public class MainTask05_topswagcode {
+
+    // GRA https://topswagcode.com/xpath/
+
+//            1 //plate
+//            2 //bento
+//            3 //plate/apple
+//            4 //*
+//            5 //*/apple
+//            6 //*[@id='fancy']
+//            7 //plate/apple
+//            8 //*[@id='fancy']/pickle
+//            9 //*[contains(@class,"small")]
+//            10 //orange[contains(@Class,"small")]
+//            11 //bento/orange[contains(@class,'small')]
+//            12 //plate|//bento
+//            13 //plate/*
+//            14 //apple/following-sibling::apple
+//            15 //plate/preceding-sibling::pickle
+//            16 (//pickle)[3]
+//            17 //plate/apple
+//            18 (//orange)[last()]|(//apple)[last()]
+//            19 //plate/apple[last()]
+//            20 (//apple)[last()-1]
+//            21 //*[@for]
+//            22 //plate[@for]
+//            23 //bento[@for="Vitaly"]
+//            24 //plate[@id='fancy' and @for='Hanna']/apple
+//            25 //*[starts-with(@for,"Sa")]
+//            26 //*[substring(@for, string-length(@for) - string-length('o') +1) = 'o']
+
+}
